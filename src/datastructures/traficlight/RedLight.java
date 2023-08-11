e860b0403c30a34a8a0f957fcf0e72b91d2ddaf3
Ganesh babu <ganeshbabugb2525@gmail.com>
2023-08-11 22:05:59 +0530

    Removed unwanted File

--- src/datastructures/traficlight/RedLight.java
+++
@@ -1,8 +0,0 @@
-package datastructures.traficlight;
-
-public final class RedLight implements TrafficLight {
-    @Override
-    public void showLight() {
-        System.out.println("RedLight is going on!");
-    }
-}
